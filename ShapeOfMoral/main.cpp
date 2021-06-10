8ab14905f4d096ff4bad45e86dfdc131be98a121
Poom Yimyuean <lebrancconvas@gmail.com>
2021-06-10 07:42:57 +0700

    :tada: Project Init.

---
+++ ShapeOfMoral/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  ShapeOfMoral
+//
+//  Created by Poom Yimyuean on 10/6/2564 BE.
+//
+
+#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+}
